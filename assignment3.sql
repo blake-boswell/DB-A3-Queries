7147930c6f973e215563c36207eb84b719ce9a9b
Blake Boswell <blake.w.boswell@gmail.com>
2018-03-06 21:31:32 -0600

    Query 2

--- assignment3.sql
+++ assignment3.sql
@@ -1,3 +1,7 @@
 -- Determine the total expense amount for each expense type
 -- OUTPUT: expense type and corresponding total amount (asc. order)
 SELECT Type, SUM(Amount) FROM Expense GROUP BY Type ORDER BY SUM(Amount) ASC;
+
+-- Show the total # of trips approved for employees of ea. department. If no employees have any approved trip, the dept. info should not appear
+-- OUTPUT: Department names and total number of trips
+SELECT Dept.Name, COUNT(Expense.Trip_ID) FROM Expense JOIN (Trip JOIN (Employee JOIN Dept ON Dept.Code = Employee.Dept) ON Trip.Emp_ID = Employee.ID) ON Expense.Trip_ID = Trip.ID GROUP BY Dept.Name; 
