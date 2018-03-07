91902371253b24d4221ec323d05983eb7a2f322b
Blake Boswell <blake.w.boswell@gmail.com>
2018-03-06 22:31:42 -0600

    Query 4

--- assignment3.sql
+++ assignment3.sql
@@ -13,3 +13,10 @@ GROUP BY Dept.Name;
 -- Show the number of distinct days when a departure and a return occur on the same day (Don't need to be from same trip)
 -- OUTPUT: Number of times we get a departure and return date occurring on the same day
 SELECT DISTINCT COUNT(*) FROM Trip A JOIN Trip B ON A.Return_Date = B.Dep_Date;
+
+-- Show the corresponding employee name, departure date, name of the city visited, and the recipt submission date for ea. approved trip
+-- OUTPUT: Employee.Name, Trip.Dep_Date, Trip.To_City, Expense.Submitted for ea. Trip in the Expense Table
+SELECT DISTINCT Employee.Name, Trip.Dep_Date, Trip.To_City, Expense.Submitted FROM Expense 
+JOIN Trip ON Expense.Trip_ID = Trip.ID 
+JOIN Employee ON Trip.Emp_ID = Employee.ID
+ORDER BY Employee.Name;
