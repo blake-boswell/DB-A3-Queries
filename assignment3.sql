c9050af49e9c392590c207e3d28bee0f297c4d3c
Blake Boswell <blake.w.boswell@gmail.com>
2018-03-06 20:23:28 -0600

    Query 1

--- assignment3.sql
+++ assignment3.sql
@@ -1,2 +1,3 @@
 -- Determine the total expense amount for each expense type
 -- OUTPUT: expense type and corresponding total amount (asc. order)
+SELECT Type, SUM(Amount) FROM Expense GROUP BY Type ORDER BY SUM(Amount) ASC;
